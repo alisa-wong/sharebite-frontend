a9e1c746766047aeaf59591e8e1a0e10ddcbeebe
Alisa Wong <aw695@cornell.edu>
2019-04-17 16:29:14 -0400

    add files

---
+++ includes/includes/init.php
@@ -0,0 +1,44 @@
+<?php
+
+function open_or_init_sqlite_db($db_filename, $init_sql_filename)
+{
+  if (!file_exists($db_filename)) {
+    $db = new PDO('sqlite:' . $db_filename);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (file_exists($init_sql_filename)) {
+      $db_init_sql = file_get_contents($init_sql_filename);
+      try {
+        $result = $db->exec($db_init_sql);
+        if ($result) {
+          return $db;
+        }
+      } catch (PDOException $exception) {
+        // If we had an error, then the DB did not initialize properly,
+        // so let's delete it!
+        unlink($db_filename);
+        throw $exception;
+      }
+    } else {
+      unlink($db_filename);
+    }
+  } else {
+    $db = new PDO('sqlite:' . $db_filename);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+  }
+  return null;
+}
+
+function exec_sql_query($db, $sql, $params = array())
+{
+  $query = $db->prepare($sql);
+  if ($query and $query->execute($params)) {
+    return $query;
+  }
+  return null;
+}
+
+$db = open_or_init_sqlite_db('secure/gallery.sqlite', 'secure/init.sql');
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('includes/init.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<header>
+
+</header>
+<body>
+    <h1>Menu</h1>
+
+    <div class='section'>
+        <
+    </div>
+
+    <div class='items'>
+    </div>
+</body>
+</html>
